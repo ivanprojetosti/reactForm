80e9f1ff81bc3a89434fb9ca0343122d9154f547
Ivan Sena <ivanprojetosti@gmail.com>
2021-03-28 19:54:50 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,30 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { RouteReuseStrategy } from '@angular/router';
+
+// import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+// import { AppComponent } from './app.component';
+// import { AppRoutingModule } from './app-routing.module';
+
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// @NgModule({
+//   declarations: [AppComponent],
+//   entryComponents: [],
+//   imports: [BrowserModule, 
+//     IonicModule.forRoot(), 
+//     AppRoutingModule, 
+//     ReactiveFormsModule, 
+//     FormsModule
+//   ], 
+//   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+//   bootstrap: [AppComponent],
+// })
+// export class AppModule {}
+
+
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -10,8 +37,15 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+
+  imports: [BrowserModule,
+      FormsModule,
+      IonicModule.forRoot(),
+      AppRoutingModule,],
+
+  providers: [{ provide: RouteReuseStrategy,
+     useClass: IonicRouteStrategy }],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../shared/shared/cliente';
+
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  cliente: Cliente;
+  constructor() { }
 
-  constructor() {}
+  ngOnInit() {
+    this.cliente = new Cliente();
+  }
 
+  onSubmit(){
+    console.log(this.cliente);
+  }
+  salvar(){
+    console.log(this.cliente);
+
+  }
 }
